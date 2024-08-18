41df85a8769bdf3fc5bfa4571bf30552b6448983
Magnesium1062 <Magnesium1062@noreply.codeberg.org>
2024-08-18 19:06:02 -0400

    Initial macOS Release

--- macos_install.sh
+++ macos_install.sh
@@ -1,10 +1,10 @@
-#! /usr/bin/env bash
+#!/bin/zsh
 
 wget https://codeberg.org/Magnesium1062/Dove/raw/branch/master/mozilla.cfg
 
 sudo mv mozilla.cfg /Applications/Thunderbird.app/Contents/Resources/mozilla.cfg
 
-wget https://codeberg.org/Magnesium1062/Dove/raw/branch/main/defaults/pref/local-settings.js
+wget https://codeberg.org/Magnesium1062/Dove/raw/branch/master/defaults/pref/local-settings.js
 
 sudo mkdir -p /Applications/Thunderbird.app/Contents/Resources/defaults/pref
 
@@ -12,4 +12,16 @@ sudo chmod 755 /Applications/Thunderbird.app/Contents/Resources/defaults/pref
 
 sudo mv local-settings.js /Applications/Thunderbird.app/Contents/Resources/defaults/pref/local-settings.js
 
-echo "Make sure to install your policies & enjoy :D"
+brew tap Magnesium1062/Dove-Policies-macOS https://codeberg.org/Magnesium1062/Dove-Policies-macOS
+
+brew update
+
+brew install dove-policies
+
+cp /opt/homebrew/opt/dove-policies/etc/thunderbird/distribution/policies.json /Applications/Thunderbird.app/Contents/Resources/distribution/policies.json
+
+echo 'alias dove-up="cp /opt/homebrew/opt/dove-policies/etc/thunderbird/distribution/policies.json /Applications/Thunderbird.app/Contents/Resources/distribution/policies.json"' >> ~/.zshrc
+
+printf "All done. :) Congratulations, you've successfully installed Dove."
+
+printf "IMPORTANT: Make sure to run the command "dove-up" after you update your policies.\nYou can set an alias in your ~/.zshrc to make this easier, such as:\nalias update='brew update && brew upgrade --force && dove-up'"
