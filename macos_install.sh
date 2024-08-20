e9a6011d4a7a791caa8c0f71c41b1f5b0499ccb3
Magnesium1062 <Magnesium1062@noreply.codeberg.org>
2024-08-20 15:12:58 -0400

    Improve macOS installer

--- macos_install.sh
+++ macos_install.sh
@@ -16,12 +16,42 @@ brew tap Magnesium1062/Dove-Policies-macOS https://codeberg.org/Magnesium1062/Do
 
 brew update
 
+brew upgrade --force
+
 brew install dove-policies
 
 cp /opt/homebrew/opt/dove-policies/etc/thunderbird/distribution/policies.json /Applications/Thunderbird.app/Contents/Resources/distribution/policies.json
+
+cat << 'EOF' > /usr/local/sbin/update_policies_dove.sh
+#!/bin/zsh
+cp /opt/homebrew/opt/dove-policies/etc/thunderbird/distribution/policies.json /Applications/Thunderbird.app/Contents/Resources/distribution/policies.json
+EOF
+
+sudo chmod +x /usr/local/sbin/update_policies_dove.sh
+
+cat << 'EOF' > ~/Library/LaunchAgents/com.user.updatepoliciesdove.plist
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+    <key>Label</key>
+    <string>com.user.updatepoliciesdove</string>
+    <key>ProgramArguments</key>
+    <array>
+        <string>/usr/local/sbin/update_policies_dove.sh</string>
+    </array>
+    <key>RunAtLoad</key>
+    <true/>
+    <key>StartInterval</key>
+    <integer>21600</integer> <!-- 6 hours in seconds -->
+</dict>
+</plist>
+EOF
+
+launchctl load ~/Library/LaunchAgents/com.user.updatepoliciesdove.plist
 
 echo 'alias dove-up="cp /opt/homebrew/opt/dove-policies/etc/thunderbird/distribution/policies.json /Applications/Thunderbird.app/Contents/Resources/distribution/policies.json"' >> ~/.zshrc
 
 printf "All done. :) Congratulations, you've successfully installed Dove."
 
-printf "IMPORTANT: Make sure to run the command "dove-up" after you update your policies.\nYou can set an alias in your ~/.zshrc to make this easier, such as:\nalias update='brew update && brew upgrade --force && dove-up'"
+printf "NOTE: Due to macOS limitations, by default, your policies will only update every 6 hours or on device boot. If you want to enforce a policies update, you can run "dove-up" after the update is downloaded download with Homebrew.\nYou can also set an alias in your ~/.zshrc to make this easier, such as:\nalias update='brew update && brew upgrade --force && dove-up'"
