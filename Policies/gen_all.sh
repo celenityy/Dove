eaf0c64a275c56a669a0ed244eb07650f3812b7f
celenity <celenity@celenity.dev>
2024-11-19 22:05:03 -0500

    Update
    Signed-off-by: celenity <celenity@celenity.dev>

--- Policies/gen_all.sh
+++ Policies/gen_all.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Replace {pdir} with the directory where Phoenix is located
-jq -s '.[0] * .[1]' base/base-policies.json {pdir}/policies/Blocklist/blocklist.json > Linux/Policies/temp.json
+# Replace ~/Projects/Phoenix with the directory where Phoenix is located
+jq -s '.[0] * .[1]' base/base-policies.json ~/Projects/Phoenix/policies/Blocklist/blocklist.json > Linux/Policies/temp.json
 
-jq -s '.[0] * .[1]' Linux/Policies/temp.json {pdir}/policies/Blocklist/cookies.json > Linux/Policies/policies.json
+jq -s '.[0] * .[1]' Linux/Policies/temp.json ~/Projects/Phoenix/policies/Blocklist/cookies.json > Linux/Policies/policies.json
 
 rm -f /Linux/Policies/temp.json
 
@@ -11,8 +11,8 @@ jq -s '.[0] * .[1]' Linux/Policies/policies.json policies-specific.json > polici
 
 jq -s '.[0] * .[1]' policies.json Personal/policies-specific.json > Personal/policies.json
 
-# Replace {fedir} with the directory where Dove-Policies-Fedora is located, otherwise you can comment this out
-cp Linux/Policies/policies.json {fedir}/policies.json
+# Replace ~/Projects/Dove-Policies-Fedora with the directory where Dove-Policies-Fedora is located, otherwise you can comment this out
+cp Linux/Policies/policies.json ~/Projects/Dove-Policies-Fedora/policies.json
 
-# Replace {dedir} with the directory where Dove-Policies-Debian is located, otherwise you can comment this out
-cp Linux/Policies/policies.json {dedir}/dove-policies/policies.json
+# Replace ~/Projects/Dove-Policies-Debian with the directory where Dove-Policies-Debian is located, otherwise you can comment this out
+cp Linux/Policies/policies.json ~/Projects/Dove-Policies-Debian/dove-policies/policies.json
