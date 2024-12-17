c6b2677564369bda15d706b7cfe85ac6d1a9c5e9
celenity <celenity@celenity.dev>
2024-12-16 22:24:27 -0500

    Ask users if they also want to remove my central repositories on Fedora & macOS upon uninstall...
    Signed-off-by: celenity <celenity@celenity.dev>

--- uninstaller_scripts/fedora_uninstall.sh
+++ uninstaller_scripts/fedora_uninstall.sh
@@ -24,4 +24,23 @@ echo_green_text "Uninstalling dove..."
 sudo dnf remove dove || error_fn
 echo
 
+read -p  $'\e[32mWould you also like to remove celenity''s COPR Repo? [Y/n] \e[0m' RESULT
+echo
+
+case ${RESULT} in
+
+		"y" | "yes" | "YES" | "Y")
+			echo_green_text "Removing celenity's COPR..."
+			sudo dnf copr remove celenity/copr || error_fn
+			echo
+
+			echo_green_text "Updating DNF cache..."
+			sudo dnf update --refresh || error_fn
+			echo
+			;;
+		
+		"n" | "no" | "N" | "NO")
+			;;
+esac
+
 echo_green_text "Thanks for giving Dove a shot. Sorry to see you go :(. Please leave feedback on how we can improve! https://dove.celenity.dev/issues"
