c51a3f4262cca1554eeedd53016181c464309cfd
celenity <celenity@celenity.dev>
2025-01-05 18:19:07 -0500

    Added missing Dove `applied` prefs to policies.json & dove.js...
    Signed-off-by: celenity <celenity@celenity.dev>

--- defaults/pref/dove.js
+++ defaults/pref/dove.js
@@ -1,5 +1,5 @@
 //
-pref("browser.dove.js.initialized", true, locked);
+//
 // Enforce that we (or any other mozilla.cfg files) do not get privileged browser access...
 // https://www.mozilla.org/firefox/62.0/releasenotes/
 pref("general.config.sandbox_enabled", true, locked);
@@ -8,4 +8,4 @@ pref("general.config.filename", "dove.cfg", locked);
 pref("general.config.vendor", "dove", locked);
 pref("general.config.obscure_value", 0);
 
-pref("browser.dove.js.applied", true, locked);
+pref("mail.dove.js.applied", true, locked);
