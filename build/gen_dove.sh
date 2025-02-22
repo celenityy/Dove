0e7be7b1824160de5a50c69d3339fa7f5957b355
celenity <celenity@celenity.dev>
2025-02-22 01:17:44 -0500

    Remove preferences from dove.cfg (like Phoenix) + update macOS build script
    Signed-off-by: celenity <celenity@celenity.dev>

--- build/gen_dove.sh
+++ build/gen_dove.sh
@@ -3,7 +3,3 @@
 # Replace ~/Projects/Phoenix with the directory where Phoenix is located
 
 cat ~/Projects/Phoenix/build/prefs/phoenix-core.js ~/Projects/Phoenix/build/prefs/phoenix-desktop-common.js ~/Projects/Phoenix/build/prefs/extended/phoenix-extended-core.js ~/Projects/Phoenix/build/prefs/extended/phoenix-extended-desktop-common.js build/prefs/dove.js > prefs/dove.js
-
-python3 build/convert.py prefs/dove.js dove.cfg
-
-awk '!/NO-OSX/' dove.cfg > macos/dove.cfg
---
+++ build/gen_macos.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python3 build/convert.py prefs/dove.js build/configs/dove-spec.cfg
+
+awk '!/NO-OSX/' build/configs/dove-spec.cfg > macos/dove.cfg
