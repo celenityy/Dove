654ea7af006a4c9e137de1a45996792febe8299e
celenity <celenity@celenity.dev>
2025-02-23 16:16:26 -0500

    Automatically update the value of `mail.dove.version`
    Signed-off-by: celenity <celenity@celenity.dev>

--- build/build.sh
+++ build/build.sh
@@ -2,4 +2,7 @@
 
 source build/env.sh
 
+# Update `mail.dove.version`
+sed -i "s/pref(\"mail.dove.version\", \".*\", locked);/pref(\"mail.dove.version\", \"$dove_version\", locked);/" build/prefs/dove.js
+
 ./build/gen_dove.sh && ./build/gen_macos.sh && ./build/gen_policies.sh && ./build/gen_archive.sh
