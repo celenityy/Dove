ef5ebfc98c034d749eb81b466878c9b68689f99e
celenity <celenity@celenity.dev>
2025-02-23 16:19:17 -0500

    Remove unnecessary environment variable
    Signed-off-by: celenity <celenity@celenity.dev>

--- build/env.sh
+++ build/env.sh
@@ -3,8 +3,5 @@
 # Where Phoenix is located
 export phoenix_dir=~/Projects/Phoenix
 
-# Current version of Dove
-export dove_previous_version=2025.02.21.1
-
 # Version of Dove you'd like to build
 export dove_version=2025.02.23.1
