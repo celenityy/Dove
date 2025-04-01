8584347f4f69a7f77a2a44c95f685a760153415b
celenity <celenity@celenity.dev>
2025-04-01 02:45:50 -0400

    Add FPP override to stop spoofing the locale reported by the Internationalization API to `en-US` by default - important for non-English speakers, and doesn't make sense since we don't spoof the `Accept-Language` HTTP header either - if anything just makes us stick out more... - `privacy.fingerprintingProtection.overrides` -> `-JSLocale`
    Signed-off-by: celenity <celenity@celenity.dev>

--- build/prefs/dove.js
+++ build/prefs/dove.js
@@ -185,7 +185,7 @@ pref("general.useragent.override", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv
 // and here: https://codeberg.org/celenity/Phoenix/wiki/Extended.md#fingerprinting
 // We're adding -HttpUserAgent & -NavigatorUserAgent (compared to standard Phoenix Extended) because they try to report that we're Firefox, which causes all kinds of breakage and weird behavior (ex. on the ATO)
 // We're removing -CanvasExtractionBeforeUserInputIsBlocked as Thunderbird simply doesn't support these permission prompts for canvas data extraction...
-pref("privacy.fingerprintingProtection.overrides", "+AllTargets,-CSSPrefersColorScheme,-FrameRate,-HttpUserAgent,-NavigatorUserAgent");
+pref("privacy.fingerprintingProtection.overrides", "+AllTargets,-CSSPrefersColorScheme,-FrameRate,-HttpUserAgent,-JSLocale,-NavigatorUserAgent");
 
 /// Reset Phoenix's FPP overrides + disable Mozilla's remote overrides
 // These are meant for browsers and may have undesired privacy implications for our use case...
