9616946d5b0ad5c7c489c9450d7bcf11e402b5eb
celenity <celenity@celenity.dev>
2025-04-17 22:18:49 -0400

    Update environment variables to disable Crash Reporting
    Signed-off-by: celenity <celenity@celenity.dev>

--- etc/profile.d/dove-env-overrides.sh
+++ etc/profile.d/dove-env-overrides.sh
@@ -15,7 +15,7 @@
 # Environment variables for GNU/Linux distros that further harden Thunderbird for Dove
 # Also impacts other Mozilla software (like Firefox)
 
-# Disable Crash Reporting
+# Disable Mozilla's Crash Reporter
 ## https://firefox-source-docs.mozilla.org/toolkit/crashreporter/crashreporter/index.html#user-specified-environment-variables
 export MOZ_CRASHREPORTER=;
 export MOZ_CRASHREPORTER_DISABLE=1;
