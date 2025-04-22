4c6c83001269297cca2ca78c58de4a3ab4a728e3
celenity <celenity@celenity.dev>
2025-04-22 15:56:36 -0400

    Fix build script to support both Linux & macOS
    Signed-off-by: celenity <celenity@celenity.dev>

--- build/build.sh
+++ build/build.sh
@@ -7,5 +7,6 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     sed -i '' "s/pref(\"mail.dove.version\", \".*\", locked);/pref(\"mail.dove.version\", \"$dove_version\", locked);/" build/prefs/dove.js
 else
     sed -i "s/pref(\"mail.dove.version\", \".*\", locked);/pref(\"mail.dove.version\", \"$dove_version\", locked);/" build/prefs/dove.js
+fi
 
 ./build/gen_dove.sh && ./build/gen_macos.sh && ./build/gen_policies.sh && ./build/gen_archive.sh
