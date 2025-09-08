d0e70e10936c711f2c744d9ad23293a9dea2a200
celenity <celenity@celenity.dev>
2025-09-07 23:49:52 -0400

    fix: Remove now-unnecessary/duplicated logic from `build.sh`
    Signed-off-by: celenity <celenity@celenity.dev>

--- build/build.sh
+++ build/build.sh
@@ -2,17 +2,11 @@
 
 source build/env.sh
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    # Update "mail.dove.version"
-    $SED -i '' "s/pref(\"mail.dove.version\", \".*\", locked);/pref(\"mail.dove.version\", \"$dove_version\", locked);/" build/dove-unified.js
-    # Update "distribution.about"
-    $SED -i '' "s/pref(\"distribution.about\", \".*\", locked);/pref(\"distribution.about\", \"Dove for Mozilla Thunderbird - $dove_version 💜\", locked);/" build/dove-unified.js
-else
-    # Update "mail.dove.version"
-    $SED -i "s/pref(\"mail.dove.version\", \".*\", locked);/pref(\"mail.dove.version\", \"$dove_version\", locked);/" build/dove-unified.js
-    # Update "distribution.about"
-    $SED -i "s/pref(\"distribution.about\", \".*\", locked);/pref(\"distribution.about\", \"Dove for Mozilla Thunderbird - $dove_version 💜\", locked);/" build/dove-unified.js
-fi
+# Update "mail.dove.version"
+$SED -i "s/pref(\"mail.dove.version\", \".*\", locked);/pref(\"mail.dove.version\", \"$dove_version\", locked);/" build/dove-unified.js
+
+# Update "distribution.about"
+$SED -i "s/pref(\"distribution.about\", \".*\", locked);/pref(\"distribution.about\", \"Dove for Mozilla Thunderbird - $dove_version 💜\", locked);/" build/dove-unified.js
 
 mkdir -vp /tmp/dove
 
