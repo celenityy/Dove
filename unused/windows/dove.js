2e6bf4aad136fcc32ca0beb76961d07953f8b13b
celenity <celenity@celenity.dev>
2025-09-15 23:13:12 -0400

    2025.09.15.1
    Signed-off-by: celenity <celenity@celenity.dev>

--- unused/windows/dove.js
+++ unused/windows/dove.js
@@ -3281,11 +3281,15 @@ pref("browser.phoenix.status.extended", "successfully applied :D", locked);
 
 // Built from Phoenix (Extended)
 
-pref("mail.dove.version", "2025.09.07.1", locked);
+pref("mail.dove.version", "2025.09.15.1", locked);
+
+/// Add custom branding at `about:support`
+pref("app.support.vendor", "Dove: 2025.09.15.1", locked); // [HIDDEN]
 
 /// Add custom branding under `Thunderbird Updates` at `about:preferences#general`
-// This will unfortunately only display if the version of Thunderbird you're using is repackaged (ex. Flatpaks/Linux distros)
-pref("distribution.about", "Dove for Mozilla Thunderbird - 2025.09.07.1 💜", locked);
+pref("distribution.about", "Dove for Mozilla Thunderbird - 2025.09.15.1 💜", locked); // [HIDDEN]
+pref("distribution.id", "default", locked); // [HIDDEN]
+pref("distribution.version", "default", locked); // [HIDDEN]
 
 /* INDEX 
 
