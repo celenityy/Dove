bad68c4f6d3ba4594273fbf211dc3db37c3a4f4c
celenity <celenity@celenity.dev>
2025-09-29 15:57:09 -0400

    feat: Run `dot_clean` before creating archives on macOS
    Signed-off-by: celenity <celenity@celenity.dev>

--- build/gen_archive.sh
+++ build/gen_archive.sh
@@ -12,11 +12,19 @@ echo_green_text() {
 
 rm -rf archives/*
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    /usr/sbin/dot_clean -mv "$dove_linux_dir"
+fi
+
 cd "$dove_linux_dir"
 
 echo_green_text "Creating archives/dove-linux.zip..."
 
 zip -r -FS "$dove_dir/archives/dove-linux.zip" *
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    /usr/sbin/dot_clean -mv "$dove_linux_flatpak_dir"
+fi
 
 cd "$dove_linux_flatpak_dir"
 
@@ -24,17 +32,29 @@ echo_green_text "Creating archives/dove-flatpak.zip..."
 
 zip -r -FS "$dove_dir/archives/dove-flatpak.zip" *
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    /usr/sbin/dot_clean -mv "$dove_osx_dir"
+fi
+
 cd "$dove_osx_dir"
 
 echo_green_text "Creating archives/dove-osx.zip..."
 
 zip -r -FS "$dove_dir/archives/dove-osx.zip" * -x 'Library/*'
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    /usr/sbin/dot_clean -mv "$dove_osx_intel_dir"
+fi
+
 cd "$dove_osx_intel_dir"
 
 echo_green_text "Creating archives/dove-osx-intel.zip..."
 
 zip -r -FS "$dove_dir/archives/dove-osx-intel.zip" * -x 'Library/*'
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    /usr/sbin/dot_clean -mv "$dove_windows_dir"
+fi
 
 cd "$dove_windows_dir"
 
