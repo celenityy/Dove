792d532b4a64008c622e62c84e2e01b0198473e1
celenity <celenity@celenity.dev>
2025-10-26 23:08:59 -0400

    2025.10.26.1
    Signed-off-by: celenity <celenity@celenity.dev>

--- build/env.sh
+++ build/env.sh
@@ -7,7 +7,7 @@ if [ -z ${phoenix_dir+x} ]; then
 fi
 
 # Version of Dove you'd like to build
-export dove_version=2025.10.15.1
+export dove_version=2025.10.26.1
 
 # Version of uBlock Origin
 export ubo_version=1.67.0
