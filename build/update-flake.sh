38dfafecf0887854c543870684fbeed7b13e039d
Jean-Baptiste Giraudeau <jb@giraudeau.info>
2025-12-01 11:19:27 +0100

    nixos: fix update-flake.sh to match proper uBlock license url
     (as fixed in #56)

--- build/update-flake.sh
+++ build/update-flake.sh
@@ -4,6 +4,6 @@ source build/env.sh
 
 # Update uBlock version in flake.nix
 $SED -i "s/download\/.*\/uBlock0_.*\.thunderbird\.xpi/download\/$ubo_version\/uBlock0_$ubo_version\.thunderbird\.xpi/" flake.nix
-$SED -i "s/uBlock\/raw\/refs\/heads\/.*\/LICENSE.txt/uBlock\/raw\/refs\/heads\/$ubo_version\/LICENSE.txt/" flake.nix
+$SED -i "s/uBlock\/refs\/tags\/.*\/LICENSE.txt/uBlock\/refs\/tags\/$ubo_version\/LICENSE.txt/" flake.nix
 
 nix flake update
